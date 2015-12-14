6c6a6590cd65601ac36dce01f13ed32a49413706
Wojciech Ziółek <wojciech.ziolek@cern.ch>
2015-12-14 15:41:57 +0100

    dojson update

--- invenio_oaiserver/dojson/oai_dc/fields/oai_dc.py
+++ invenio_oaiserver/dojson/oai_dc/fields/oai_dc.py
@@ -23,114 +23,92 @@
 """OAI DublinCore model definition."""
 
 from dojson import utils
-from ..model import hep2marc
+from ..model import hep2oai_dc
 
 
-@hep2marc.over('024', '^(dois|persistent_identifiers)$')
+@hep2oai_dc.over('identifier', '^(dois|persistent_identifiers)$')
 def dois2marc(self, key, value):
     """Other Standard Identifier."""
     value = utils.force_list(value)
 
     def get_value(val):
         return {
-            'a': val.get('value'),
-            '9': val.get('source'),
-            '2': val.get('type') or "DOI"
+            val.get('value')
         }
 
-    self['024'] = self.get('024', [])
+    self['identifier'] = self.get('identifier', [])
     for val in value:
-        self['024'].append(get_value(val))
-    return self['024']
+        self['identifier'].append(get_value(val))
+    return self['identifier']
 
 
-@hep2marc.over('035', 'external_system_numbers')
-@utils.for_each_value
-@utils.filter_values
-def external_system_numbers2marc(self, key, value):
-    """System Control Number."""
-    return {
-        'a': value.get('value'),
-        '9': value.get('institute'),
-        'z': value.get('obsolete'),
-    }
-
-
-@hep2marc.over('100', '^authors$')
+@hep2oai_dc.over('creator', '^authors$')
 def authors2marc(self, key, value):
     """Main Entry-Personal Name."""
     value = utils.force_list(value)
 
     def get_value(value):
-        affiliations = [
-            aff.get('value') for aff in value.get('affiliations', [])
-        ]
         return {
-            'a': value.get('full_name'),
-            'e': value.get('role'),
-            'q': value.get('alternative_name'),
-            'i': value.get('inspire_id'),
-            'j': value.get('orcid'),
-            'm': value.get('email'),
-            'u': affiliations,
-            'x': value.get('recid'),
-            'y': value.get('claimed')
+            value.get('full_name'),
         }
-
-    if len(value) > 1:
-        self["700"] = []
-    for author in value[1:]:
-        self["700"].append(get_value(author))
-    return get_value(value[0])
-
-@hep2marc.over('210', '^title_variation$')
-@utils.for_each_value
-@utils.filter_values
-def title_variation2marc(self, key, value):
-    """Title variation."""
-    return {
-        'a': value,
-    }
+    self["creator"] = self.get('creator', [])
+    for author in value:
+        self["creator"].append(get_value(author))
+    return self["creator"]
 
 
-@hep2marc.over('245', '^titles$')
-@utils.for_each_value
-@utils.filter_values
-def titles2marc(self, key, value):
-    """Title Statement."""
-    return {
-        'a': value.get('title'),
-        'b': value.get('subtitle'),
-        '9': value.get('source'),
-    }
+# @hep2oai_dc.over('title', '^titles$')
+# @utils.for_each_value
+# @utils.filter_values
+# def titles2marc(self, key, value):
+#     """Title Statement."""
+#     return {
+#         value.get('title'),
+#     }
 
-@hep2marc.over('520', 'abstracts')
-@utils.for_each_value
-@utils.filter_values
-def abstract2marc(self, key, value):
-    """Summary, Etc.."""
-    return {
-        'a': value.get('value'),
-        '9': value.get('source'),
-    }
 
-@hep2marc.over('773', 'publication_info')
-@utils.for_each_value
-@utils.filter_values
-def publication_info2marc(self, key, value):
-    """Publication info about record."""
-    return {
-        '0': value.get('recid'),
-        'c': value.get('page_artid'),
-        'n': value.get('journal_issue'),
-        'o': value.get('conf_acronym'),
-        'p': value.get('journal_title'),
-        'r': value.get('reportnumber'),
-        't': value.get('confpaper_info'),
-        'v': value.get('journal_volume'),
-        'w': value.get('cnum'),
-        'x': value.get('pubinfo_freetext'),
-        'y': value.get('year'),
-        'z': value.get('isbn'),
-        'm': value.get('note')
-    }
+# @hep2oai_dc.over('abstract', 'abstracts')
+# @utils.for_each_value
+# @utils.filter_values
+# def abstract2marc(self, key, value):
+#     """Summary, Etc.."""
+#     value = utils.force_list(value)
+#     def get_value(value):
+#         return {
+#             value.get('value')
+#         }
+#     self['abstract'] = self.get('abstract', [])
+#     for abst in value:
+#         self['abstract'].append(get_value(abst))
+#     return self['abstract']
+
+# TODO
+# @hep2oai_dc.over('date', 'earliest_date')
+# @utils.for_each_value
+# @utils.filter_values
+# def abstract2marc(self, key, value):
+#     """Summary, Etc.."""
+#     return {
+#         value
+#     }
+
+# @hep2oai_dc.over('publisher', 'publication_info')
+# @utils.for_each_value
+# @utils.filter_values
+# def publication_info2marc(self, key, value):
+#     """Publication info about record."""
+#     return {
+#         '0': value.get('recid'),
+#         'c': value.get('page_artid'),
+#         'n': value.get('journal_issue'),
+#         'o': value.get('conf_acronym'),
+#         'p': value.get('journal_title'),
+#         'r': value.get('reportnumber'),
+#         't': value.get('confpaper_info'),
+#         'v': value.get('journal_volume'),
+#         'w': value.get('cnum'),
+#         'x': value.get('pubinfo_freetext'),
+#         'y': value.get('year'),
+#         'z': value.get('isbn'),
+#         'm': value.get('note')
+#     }
